d4051bde4fd9a517485f07ccf2623438dc6bd435
Rass <32880364+rass2016@users.noreply.github.com>
2018-04-21 11:34:57 -0300

    done player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,24 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float speed = 5f;
+    Rigidbody2D rgbd2D;
+    private float speed = 250f;
     Vector2 currentPosition = new Vector2();
     Vector2 velocity = new Vector2();
     Vector2 counterVelocity = new Vector2();
     Vector2 motionless = new Vector2(0, 0);
 	// Use this for initialization
-	void Start () {
-
+	void Start()
+    {
+        rgbd2D = GetComponent<Rigidbody2D>();
 	}
 	
     void OnCollisionEnter2D(Collision2D collision)
     {   
-        // each collider probably will trigger its own collisionenter event
-        Collider2D collider = collision.collider;
-        //collider.offset
-        //idea : if i know the offset of the collider I can determine
-        // the direction in which a counterVelocity should be applied to
-        // this counter velocity vector should be calculated with the normal velocity
         print("collision detected");
     }
 	// Update is called once per frame
@@ -33,7 +29,8 @@ public class PlayerMovement : MonoBehaviour {
 
     void MovementInput()
     {
-        currentPosition = transform.position;
+        velocity = motionless;
+        //currentPosition = transform.position;
 		if(Input.GetKey(KeyCode.D))
         {
             velocity.x += speed * Time.deltaTime;
@@ -50,8 +47,7 @@ public class PlayerMovement : MonoBehaviour {
         {
             velocity.y -= speed * Time.deltaTime;
         }
-        
-        transform.position = currentPosition + velocity;
-        velocity = motionless;
+        rgbd2D.velocity = velocity;
+        //transform.position = currentPosition + velocity;
     }
 }
